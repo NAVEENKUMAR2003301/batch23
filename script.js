05deeb5cc1affe13ce76962b8b41fedbdff64321
NAVEENKUMAR2003301 <livnaveen@gmail.com>
2025-12-11 13:37:25 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,186 @@
+// // variable 
+
+// // var   - global scope
+
+// // let   - block scope
+
+// // const - block scope
+
+
+// // "javaScript"
+
+
+
+// // rules 
+
+// // var - GB
+
+
+// var chocolateBox = 10   // declration // intialiazation
+
+// chocolateBox     = 15   // reuse    // reIntialiazation
+
+// var chocolateBox = 30   // reDeclration
+
+// console.log(chocolateBox)
+
+
+// // let 
+
+// let a = 50      // dec   // intia
+
+// a     = 60      // reuse // reInt
+
+// // let a = 70   // not accept reDeclra
+
+// console.log(a);
+
+
+// // const 
+
+// const b = 50  // dec // intia
+
+// // b = 60     // reUse // ReIntia
+
+// // const b = 70  // reDeclration
+
+// console.log(b);
+
+
+// var a = 10;
+// var a = 20;
+// console.log(a);  // 20 
+
+
+// let x = 5;
+// x = 8;
+// console.log(x);  // 8 
+
+
+// const p = 30;
+// // p = 40;
+// console.log(p);  // 30 
+
+
+// var num = 100;
+// num = 200;
+// console.log(num); // 200 
+
+
+// let count = 1;
+// // let count = 2;
+// console.log(count);  // 1
+
+
+// const rate = 50;
+// console.log(rate); // 50
+
+
+// var a = 10;
+// a = 15;
+// var a = 25;
+// console.log(a);  // 25
+
+
+// const c = 10;
+// c = 15;
+// console.log(c);  // error - typeError
+
+
+// var price = 99;
+// price = 199;
+// console.log(price);  // 199
+
+
+// let mobile = 5000;
+// mobile = 6000;
+// console.log(mobile);  // 6000
+
+
+// var car = "BMW";
+// var car = "Audi";
+// console.log(car);    // Audi
+
+
+// let fruit = "Apple";
+// let fruit = "Mango";
+// console.log(fruit);   // error - syntex error 
+
+
+// const city = "Delhi";
+// city = "Mumbai";
+// console.log(city);   // error - typeError
+
+
+// var x = 1;
+// x = 2;
+// x = 3;
+// console.log(x);  // 3
+
+
+// let y = 100;
+// y = 150;
+// console.log(y);  // 150
+
+
+
+// printing statements
+
+// 1. console.log()
+
+console.log(100);
+
+let a = 50
+
+console.log(a);
+
+//2. alert()
+
+// alert("hello guys this our first class!!!")
+
+//3. confirm()
+
+// confirm("did you like apple !")
+
+//4. prompt()
+
+// prompt("what is your name")
+
+
+//5. document.writeln()
+
+// document.writeln("hello")
+
+
+// i want to print my name in console 
+
+console.log("ben");
+
+// i want to print ben name in ui Side
+
+document.writeln("ben")
+
+// i want to show hi message in ui side in pop way
+
+// alert("hi")
+
+// i want know suggestion from user ("did  u like apple")
+
+// let userWish = confirm("did u like apple")
+
+// console.log(userWish);
+
+
+// i want to know user name
+
+let name1 = prompt("enter your name :-")
+
+console.log(name1);
+
+
+
+
+
+
+
+
